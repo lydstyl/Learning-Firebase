5857ee5d39466f78a035512ce802dca47f688abd
Gabriel Brun <lydstyl@gmail.com>
2018-10-27 20:40:43 +0200

    add getRealtimeUpdates with firestore

--- js.js
+++ js.js
@@ -16,18 +16,45 @@ const docRef = firestore.doc("samples/sandwichData");
 const outputHeader = document.querySelector("#hotDogOutput");
 const inputTextField = document.querySelector("#latestHotDogStatus");
 const saveButton = document.querySelector("#saveButton");
+const loadButton = document.querySelector("#loadButton");
 
-saveButton.addEventListener("click", function () {
+function getRealtimeUpdates(doc) {
+    docRef.onSnapshot(doc =>{
+        updateHeader(doc);
+    });
+}
+
+function updateHeader(doc) {
+    if (doc && doc.exists) {
+        const myData = doc.data();
+        outputHeader.innerHTML = "Hot dog status: " + myData.hotDogStatus;
+    }
+}
+
+saveButton.addEventListener("click", () => {
     const textToSave = inputTextField.value;
     console.log("I am going to save " + textToSave + "to Firestore");
     docRef
         .set({
             hotDogStatus: textToSave
         })
-        .then( function () {
+        .then( () => {
             console.log("Status saved!");
         })
-        .catch(function (error) {
+        .catch(error => {
             console.log("Got an error: ", error);
         });
-})
+});
+
+loadButton.addEventListener("click", () => {
+    docRef
+        .get()
+        .then(doc => {
+            updateHeader(doc);
+        })
+        .catch( error => {
+            console.log("Got an error: ", error);
+        });
+});
+
+getRealtimeUpdates();
