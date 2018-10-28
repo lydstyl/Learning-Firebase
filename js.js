568e61b5fdd0369d2d85ffb0c77da6f9801f4268
Gabriel Brun <lydstyl@gmail.com>
2018-10-28 15:29:01 +0100

    reset password ok todo return to website after reset pass

--- js.js
+++ js.js
@@ -16,6 +16,7 @@ const docRef = firestore.doc("samples/sandwichData");
 const txtEmail = document.getElementById("txtEmail"); // coco@coco.com
 const txtPassword = document.getElementById("txtPassword");
 const btnLogin = document.getElementById("btnLogin");
+const btnResetPassword = document.getElementById("btnResetPassword");
 const btnSignUp = document.getElementById("btnSignUp");
 const btnLogout = document.getElementById("btnLogout");
 const log = document.getElementById("log");
@@ -50,13 +51,25 @@ function loadDocRef() {
 }
 
 // log in s'identifier; sign in se connecter; sign up s'inscrire; sign out déconnexion
+//btnResetPassword.classList.remove('hide');
+btnResetPassword.addEventListener("click", e => {
+    e.preventDefault();
+    let email = txtEmail.value;
+    firebase.auth().sendPasswordResetEmail(email)
+});
+
 btnLogin.addEventListener("click", e =>{
     // TODO: CHECK FOR REAL EMAIL
     const email = txtEmail.value;
     const pass = txtPassword.value;
     const auth = firebase.auth();
     const promise = auth.signInWithEmailAndPassword(email, pass);
-    promise.catch(e => log.innerHTML = e.message);
+    promise.catch(e => {
+        log.innerHTML = e.message
+        if (e.message == "The password is invalid or the user does not have a password.") {
+            btnResetPassword.classList.remove('hide');
+        }
+    });
     if (outputHeader.innerText == "Hot dog status:") {
         console.log("TODO");
         //getRealtimeUpdates();
@@ -79,6 +92,9 @@ btnLogout.addEventListener("click", e => {
 });
 firebase.auth().onAuthStateChanged(firebaseUser => {
     if (firebaseUser) {
+        if (firebaseUser.email) {
+            btnResetPassword.classList.add('hide');
+        }
         log.innerHTML = "logged with " + firebaseUser.email;
         btnLogin.classList.add("hide");
         btnSignUp.classList.add("hide");
@@ -108,3 +124,33 @@ loadButton.addEventListener("click", () => {
     loadDocRef();
 });
 getRealtimeUpdates();
+
+
+
+// var actionCodeSettings = {
+//     url: 'https://www.example.com/?email=user@example.com',
+//     iOS: {
+//         bundleId: 'com.example.ios'
+//     },
+//     android: {
+//         packageName: 'com.example.android',
+//         installApp: true,
+//         minimumVersion: '12'
+//     },
+//     handleCodeInApp: true
+// };
+// firebase.auth().sendPasswordResetEmail(
+//     'user@example.com', actionCodeSettings)
+//     .then(function() {
+//         // Password reset email sent.
+//     })
+//     .catch(function(error) {
+//         // Error occurred. Inspect error.code.
+//     });
+
+
+// todo email  vérification and reset password
+
+// todo 2 pages toujour loggé ?
+
+// todo recevoir un email quand qqn se connecte sur mon cv ?
