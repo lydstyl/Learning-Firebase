189701e170ade31cfc6db59a02f3c821f101d631
Gabriel Brun <lydstyl@gmail.com>
2018-10-28 12:08:49 +0100

    ok to do forget pass

--- js.js
+++ js.js
@@ -13,64 +13,86 @@ firestore.settings(settings);
 
 const docRef = firestore.doc("samples/sandwichData");
 
-const txtEmail = document.querySelector("#txtEmail");
-const txtPassword = document.querySelector("#txtPassword");
-const btnLogin = document.querySelector("#btnLogin");
-const btnSignUp = document.querySelector("#btnSignUp");
-const btnLogout = document.querySelector("#btnLogout");
+const txtEmail = document.getElementById("txtEmail"); // coco@coco.com
+const txtPassword = document.getElementById("txtPassword");
+const btnLogin = document.getElementById("btnLogin");
+const btnSignUp = document.getElementById("btnSignUp");
+const btnLogout = document.getElementById("btnLogout");
+const log = document.getElementById("log");
 
-const outputHeader = document.querySelector("#hotDogOutput");
-const inputTextField = document.querySelector("#latestHotDogStatus");
-const saveButton = document.querySelector("#saveButton");
-const loadButton = document.querySelector("#loadButton");
+const main = document.querySelector("main");
+const outputHeader = document.getElementById("hotDogOutput");
+const inputTextField = document.getElementById("latestHotDogStatus");
+const saveButton = document.getElementById("saveButton");
+const loadButton = document.getElementById("loadButton");
 
 function getRealtimeUpdates(doc) {
-    docRef.onSnapshot(doc =>{
-        updateHeader(doc);
-    });
+    docRef
+        .onSnapshot(doc =>{
+            updateHeader(doc);
+        });
 }
-
 function updateHeader(doc) {
     if (doc && doc.exists) {
         const myData = doc.data();
         outputHeader.innerHTML = "Hot dog status: " + myData.hotDogStatus;
     }
 }
+function loadDocRef() {
+    docRef
+        .get()
+        .then(doc => {
+            updateHeader(doc);
+        })
+        .catch( error => {
+            console.log("Got an error: ", error);
+        });
+}
+
 // log in s'identifier; sign in se connecter; sign up s'inscrire; sign out déconnexion
-// toto@toto.com
 btnLogin.addEventListener("click", e =>{
     // TODO: CHECK FOR REAL EMAIL
     const email = txtEmail.value;
     const pass = txtPassword.value;
     const auth = firebase.auth();
     const promise = auth.signInWithEmailAndPassword(email, pass);
-    promise.catch(e => console.log(e.message));
+    promise.catch(e => log.innerHTML = e.message);
+    if (outputHeader.innerText == "Hot dog status:") {
+        console.log("TODO");
+        //getRealtimeUpdates();
+        //loadDocRef();
+        // reload to get the loadDocRef
+        // location.reload();
+    }
 });
 btnSignUp.addEventListener("click", e =>{
     const email = txtEmail.value;
     const pass = txtPassword.value;
     const auth = firebase.auth();
     const promise = auth.createUserWithEmailAndPassword(email, pass);
-    promise.catch(e => console.log(e.message));
+    promise.catch(e => log.innerHTML = e.message);
 });
 btnLogout.addEventListener("click", e => {
     firebase.auth().signOut();
+    btnLogin.classList.remove("hide");
+    btnSignUp.classList.remove("hide");
 });
 firebase.auth().onAuthStateChanged(firebaseUser => {
-    console.log("onAuthStateChanged");
-    
     if (firebaseUser) {
-        console.log(firebaseUser);
+        log.innerHTML = "logged with " + firebaseUser.email;
+        btnLogin.classList.add("hide");
+        btnSignUp.classList.add("hide");
         btnLogout.classList.remove("hide");
+        main.classList.remove("hide");
     } else {
-        console.log('not logged in');
+        log.innerHTML = 'not logged in';
         btnLogout.classList.add("hide");
+        main.classList.add("hide");
     }
 });
-
 saveButton.addEventListener("click", () => {
     const textToSave = inputTextField.value;
-    console.log("I am going to save " + textToSave + "to Firestore");
+    console.log("I am going to save " + textToSave + " to Firestore");
     docRef
         .set({
             hotDogStatus: textToSave
@@ -82,16 +104,7 @@ saveButton.addEventListener("click", () => {
             console.log("Got an error: ", error);
         });
 });
-
 loadButton.addEventListener("click", () => {
-    docRef
-        .get()
-        .then(doc => {
-            updateHeader(doc);
-        })
-        .catch( error => {
-            console.log("Got an error: ", error);
-        });
+    loadDocRef();
 });
-
 getRealtimeUpdates();
